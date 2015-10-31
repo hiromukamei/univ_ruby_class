ef3feb9e0e148ec50f0e789f633d8e7dad952d98
Hiromu Kamei <hk.radwimps@gmail.com>
2015-10-31 21:26:19 +0900

    Add 'kadai13.rb' [New]
    Done kadai13 completely

---
+++ kadai13.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+class Student
+    attr_reader :name
+
+	def initialize(n)
+		@name = n
+	end
+
+	def affiliation
+		return university + faculty
+	end
+end
+
+class KeioStudent < Student
+	def university
+		return '慶應義塾大学'
+	end
+end
+
+class PStudent < KeioStudent
+	def faculty
+		return '総合政策学部'
+	end
+end
+
+class EStudent < KeioStudent
+	def faculty
+		return '環境情報学部'
+	end
+end
+
+taro = PStudent.new('太郎')
+hanako = EStudent.new('花子')
+puts "#{taro.name}は#{taro.affiliation}の学生です。"
+puts "#{hanako.name}は#{hanako.affiliation}の学生です。"
